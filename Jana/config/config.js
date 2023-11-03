427dd9f99aecfee21f18bbcee676c8c9d9401331
janakw <120371341+janakw@users.noreply.github.com>
2023-11-03 12:28:40 +0100

    animation start

---
+++ Jana/config/config.js
@@ -0,0 +1,14 @@
+console.log ("file_executed: config.js")
+
+console.log("Initialisierung Supabase");
+
+// Supabase Initialisierung
+const supabaseUrl = 'https://xgqaqvefiaydtcmvogvh.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhncWFxdmVmaWF5ZHRjbXZvZ3ZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU2NDA0MzgsImV4cCI6MjAxMTIxNjQzOH0.zBg2S04RQnARDvGf6covdLiey7CRds0K7yU_kMzR3Ao'
+const supa = supabase.createClient(supabaseUrl, supabaseKey, {
+    auth: {
+        redirectTo: window.location.origin,  // This will redirect back to the page where the request originated from
+    },
+});
+
+export { supa }
---
+++ Jana/scripts/swipeGo.js
@@ -0,0 +1,14 @@
+console.log ("file_executed: swipeGo.js");
+
+let buttonSwipe = document.querySelector("#buttonSwipe");
+let toggleSwipe = false;  /* button links ist false */
+
+/* 
+addEventListener
+toggleSwipe variable erstellen
+ */
+
+
+buttonSwipe.addEventListener(click)
+
+wenn variable false ist muss classe swipe go hinzugefügt werden wenn true class swipestopp hinzufügen
